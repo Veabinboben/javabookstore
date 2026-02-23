a41e1e2c7c5575d67e753091dce0bba0194e2055
Ivan <veab04@gmail.com>
2026-02-23 15:27:50 +0300

    feat(consoleApp): added database connection

--- jdbc_test/src/main/java/com/example/jdbc_test/Main.java
+++ jdbc_test/src/main/java/com/example/jdbc_test/Main.java
@@ -1,10 +1,59 @@
 package com.example.jdbc_test;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.xml.crypto.Data;
 
 public class Main {
+    public static Connection connect() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/javadb";
+        String user = "postgres";
+        String password = "admin";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL database successfully.");
+            return connection;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw e; 
+        }
+    }
+
+    public static void GetTables(Connection conn) throws  SQLException{
+            String[] typs = {"TABLE"};
+            DatabaseMetaData meta = conn.getMetaData();
+            ResultSet tables = meta.getTables(null, null, null, typs);
+            while (tables.next()){
+                ResultSet columns = meta.getColumns(null, null, tables.getString("TABLE_NAME"), "%");
+                System.out.println(" <-> " + tables.getString("TABLE_NAME"));
+                while (columns.next()) {
+                    String columnName = columns.getString("COLUMN_NAME");
+                    String columnType = columns.getString("TYPE_NAME");
+                    int columnSize = columns.getInt("COLUMN_SIZE");
+                    System.out.println("  - " + columnName + " (" + columnType + ", Size: " + columnSize + ")");
+                }
+            }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection conn = connect()) {
+            // PreparedStatement books_insert = conn.prepareStatement(
+            //     "INSERT INTO books (title, publish_date, price) VALUES (?, ?, ?)"
+            // );
+            GetTables(conn);
+
+            
+
+        } catch (SQLException e) {
+            System.err.println("Database connection failed.");
+            e.printStackTrace();
+        }
     }
 }
