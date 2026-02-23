24ffd0e3116b1781da0c5c18ed5ad40f5c055b16
Ivan <veab04@gmail.com>
2026-02-23 12:58:58 +0300

    Init commit

---
+++ jdbc_test/src/main/java/com/example/jdbc_test/Main.java
@@ -0,0 +1,10 @@
+package com.example.jdbc_test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
